02c6cd7f989bde67260791822a462ed111789a38
kasmiamjad <kasmi.amjad@gmail.com>
2026-07-02 17:02:49 +0530

    Instagram: call graph.instagram.com (Instagram Login tokens) not graph.facebook.com
    The app uses the 'Instagram API with Instagram Login' setup, whose access tokens are only valid on graph.instagram.com. Sending to graph.facebook.com returned 'Cannot parse access token' and broke DM sends + profile-name lookups. Point IG_GRAPH_BASE_URL at graph.instagram.com.

--- src/services/platforms/instagram/instagram.constants.ts
+++ src/services/platforms/instagram/instagram.constants.ts
@@ -1,2 +1,5 @@
 export const IG_GRAPH_API_VERSION = "v22.0";
-export const IG_GRAPH_BASE_URL = `https://graph.facebook.com/${IG_GRAPH_API_VERSION}`;
+// "Instagram API with Instagram Login" tokens are only valid against
+// graph.instagram.com. Using graph.facebook.com with those tokens returns
+// "Invalid OAuth access token - Cannot parse access token".
+export const IG_GRAPH_BASE_URL = `https://graph.instagram.com/${IG_GRAPH_API_VERSION}`;
